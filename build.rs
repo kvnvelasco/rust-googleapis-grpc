1eacd649f1a114f585b9fedf976eb470bf1fd73b
kevin <me@kvnvelasco.com>
2020-03-30 12:25:10 +0800

    add base configuration

---
+++ build.rs
@@ -0,0 +1,12 @@
+use tonic_build::configure;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    configure()
+        .build_server(false)
+        .out_dir("./src/protodefs")
+        .compile(
+            &["googleapis-src/google/firestore/v1/firestore.proto"],
+            &["googleapis-src"],
+        )?;
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod google {
+    pub mod firestore {
+        pub mod v1 {
+            include!("protodefs/google.firestore.v1.rs");
+        }
+    }
+    pub mod rpc {
+        include!("protodefs/google.rpc.rs");
+    }
+    pub mod r#type {
+        include!("protodefs/google.r#type.rs");
+    }
+}
